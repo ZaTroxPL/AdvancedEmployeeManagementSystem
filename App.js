adc4a81fb5de992803caad43f1e64056c600994c
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-12-30 21:11:53 +0000

    Made a basic structure for the home page of the app

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Employee Management System</Text>
+      </View>
+      <EmployeeDetails />
+      <EmployeeInteractions />
+    </ScrollView>
+  );
+}
+
+export function EmployeeDetails() {
+  const salary = 23000;
+  const holidaysLeft = 1;
+  const leaveStatus = true;
+  const employeeName = "Jakub Panczyszyn";
+
+  return (
+    <View>
+      <View style={[styles.container, styles.column]}>
+        <Text style={styles.valueDisplay}>
+          Name: {employeeName}
+        </Text>
+        <Text style={styles.valueDisplay}>
+          Salary: £{salary}
+        </Text>
+        <Text style={styles.valueDisplay}>
+          Leave Status: {leaveStatus ? "On Leave" : "At Work"}
+        </Text>
+        <Text style={styles.valueDisplay}>
+          Holidays Left: {holidaysLeft} {holidaysLeft > 1 || holidaysLeft < 1 ? "days" : "day"}
+        </Text>
+      </View>
     </View>
   );
 }
 
+export function EmployeeInteractions() {
+  return (
+    <View style={[styles.container, styles.flexWrap, styles.row]}>
+      <View style={styles.button}>
+        <Button title="Submit Holiday Request" />
+      </View>
+      <View style={styles.button}>
+        <Button title="Report Complaint" />
+      </View>
+      <View style={styles.button}>
+        <Button title="I Want to Resign" />
+      </View>
+    </View>
+  );
+}
+
+
 const styles = StyleSheet.create({
+  scrollContainer: {
+    padding: 15
+  },
+  flexWrap: {
+    flexWrap: 'wrap'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  column: {
+    flexDirection: 'column'
+  },
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 15,
+    marginBottom: 15,
   },
+  heading: {
+    fontSize: '2em',
+    fontWeight: 'bold'
+  },
+  valueDisplay: {
+    margin: 15,
+    fontSize: '1.25em',
+    fontWeight: 'bold'
+  },
+  button: {
+    margin: 15
+  }
 });
