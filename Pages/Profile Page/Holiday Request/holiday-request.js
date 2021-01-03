6fe12cef24006d9491370eb10ceb797f0b0c5f3c
Jakub Panczyszyn <zatroxkp@gmail.com>
2021-01-03 21:59:53 +0000

    Finished Holiday Request Page

--- Pages/Profile Page/Holiday Request/holiday-request.js
+++ Pages/Profile Page/Holiday Request/holiday-request.js
@@ -2,21 +2,48 @@ import React, { useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, Button, Platform } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
+import { set } from 'react-native-reanimated';
 
 
 export function HolidayRequest({ navigation }) {
+    const [text, setText] = useState("");
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
     return (
-        <ScrollView style={styles.scrollContainer}>
-            <StartEndDatePickers />
-            <TextInput multiline={true} style={styles.textInput} />
-            <View>
-                <Button title="Submit" />
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View
+                style={StyleSheet.create({
+                    flex: 1
+                })}
+            >
+                <StartEndDatePickers
+                    setParentStartDate={setStartDate}
+                    setParentEndDate={setEndDate}
+                />
             </View>
+            <View
+                style={StyleSheet.create({
+                    flex: 99
+                })}
+            >
+                <TextInput 
+                    multiline={true} 
+                    style={styles.textInput} 
+                    onChangeText={text => setText(text)}                    
+                />                
+                <Button 
+                    title="Submit"
+                    onPress={function () {                        
+                        // TODO: Send text, start and end date to the database
+                    }} 
+                />
+            </View>           
         </ScrollView>
     );
 }
 
-function StartEndDatePickers() {
+function StartEndDatePickers(prop) {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [show, setShow] = useState(false);
@@ -26,9 +53,11 @@ function StartEndDatePickers() {
         setShow(Platform.OS === 'ios');
         if (dateType === "start") {
             setStartDate(selectedDate);
+            prop.setParentStartDate(selectedDate);
         }
         else if (dateType === "end") {
-            setEndDate(selectedDate)
+            setEndDate(selectedDate);
+            prop.setParentEndDate(selectedDate);
         }
     };
 
@@ -42,15 +71,48 @@ function StartEndDatePickers() {
         setDateType("end");
     };
 
+    function clearStartDate() {
+        setStartDate(null);
+        prop.setParentStartDate(null);
+    }
+
+    function clearEndDate() {
+        setEndDate(null);
+        prop.setParentEndDate(null);
+    }
+
     return (
         <View>
-
-            <View style={styles.row}>
+            <View style={[styles.row, styles.datePicker]}>
                 <View>
-                    <Button onPress={showStartDatePicker} title="Pick Start Date" />
+                    <View style={styles.standardRow}>
+                        <Button onPress={showStartDatePicker} title="Pick Start Date" />
+                    </View>
+                    <View style={[styles.standardRow, styles.dateDisplay, styles.selfAlignStart]}>
+                        <View style={styles.cancelButtonLeft}>
+                            <Button onPress={clearStartDate} title="X" />
+                        </View>
+                        {startDate && (
+                            <Text>
+                                {startDate?.getDate() + "/" + (startDate?.getMonth() + 1) + "/" + startDate?.getFullYear()}
+                            </Text>
+                        )}
+                    </View>
                 </View>
                 <View>
-                    <Button onPress={showEndDatePicker} title="Pick End Date" />
+                    <View style={styles.standardRow}>
+                        <Button onPress={showEndDatePicker} title="Pick End Date" />
+                    </View>
+                    <View style={[styles.standardRow, styles.dateDisplay, styles.selfAlignEnd]}>                        
+                        {endDate && (
+                            <Text>
+                                {endDate?.getDate() + "/" + (endDate?.getMonth() + 1) + "/" + endDate?.getFullYear()}
+                            </Text>
+                        )}
+                        <View style={styles.cancelButtonRight}>
+                            <Button onPress={clearEndDate} title="X" />
+                        </View>
+                    </View>
                 </View>
                 {show && (
                     <RNDateTimePicker
@@ -62,32 +124,47 @@ function StartEndDatePickers() {
                     />
                 )}
             </View>
-            <View style={styles.row}>
-                <View>
-                    {startDate && (<Text>{startDate?.getDate() + "/" + (startDate?.getMonth() + 1) + "/" + startDate?.getFullYear()}</Text>)}
-                </View>
-                <View>
-                    {endDate && (<Text>{endDate?.getDate() + "/" + (endDate?.getMonth() + 1) + "/" + endDate?.getFullYear()}</Text>)}
-                </View>
-            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     scrollContainer: {
-        padding: 15
+        padding: 15,
+        flexGrow: 1
     },
     textInput: {
         backgroundColor: 'white',
         borderColor: 'black',
         borderWidth: 1,
-        height: 90,
+        alignItems: 'stretch',
+        flex: 1
     },
     row: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
         marginBottom: 15,
         marginTop: 15,
     },
+    standardRow: {
+        flexDirection: 'row'
+    },
+    datePicker: {
+        justifyContent: 'space-between'
+    },
+    dateDisplay: {
+        marginTop: 15,
+        alignItems: 'center'
+    },
+    selfAlignStart: {
+        alignSelf: 'flex-start'
+    },
+    selfAlignEnd: {
+        alignSelf: 'flex-end'
+    },
+    cancelButtonLeft: {
+        marginRight: 10
+    },
+    cancelButtonRight: {
+        marginLeft: 10
+    }
 });
